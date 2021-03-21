36fe280f75e90503d1ce87412fdb9de464da643a
cheshir <cheshirysh@gmail.com>
2021-03-22 00:14:45 +0200

    Added more benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,19 +7,41 @@ import (
 
 // The Set time is highly depends on the map capacity.
 // The main problem is map expansion.
-func BenchmarkCache_Set(b *testing.B) {
+func BenchmarkCache_Set_100(b *testing.B) {
+	benchmarkCacheSet(b, 100)
+}
+
+func BenchmarkCache_Set_1000(b *testing.B) {
+	benchmarkCacheSet(b, 1000)
+}
+
+func BenchmarkCache_Set_10000(b *testing.B) {
+	benchmarkCacheSet(b, 10000)
+}
+
+func BenchmarkCache_Get_100(b *testing.B) {
+	benchmarkCacheGet(b, 100)
+}
+
+func BenchmarkCache_Get_1000(b *testing.B) {
+	benchmarkCacheGet(b, 1000)
+}
+
+func BenchmarkCache_Get_10000(b *testing.B) {
+	benchmarkCacheGet(b, 10000)
+}
+
+func benchmarkCacheSet(b *testing.B, numberOfKeys int) {
 	resolution := 9999 * time.Second // Avoid stopping the world.
-	uniqueKeysCount := 100
 	c := New(resolution)
 
 	for i := 0; i < b.N; i++ {
-		c.Set(IntKey(i%uniqueKeysCount), i, 0)
+		c.Set(IntKey(i%numberOfKeys), i, 0)
 	}
 }
 
-func BenchmarkCache_Get(b *testing.B) {
+func benchmarkCacheGet(b *testing.B, numberOfKeys int) {
 	resolution := 9999 * time.Second // Avoid stopping the world.
-	uniqueKeysCount := 100
 	c := New(resolution)
 
 	for i := 0; i < 100; i++ {
@@ -29,7 +51,7 @@ func BenchmarkCache_Get(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		val, _ := c.Get(IntKey(i % uniqueKeysCount))
+		val, _ := c.Get(IntKey(i % numberOfKeys))
 		_ = val
 	}
 }
